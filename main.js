c04a3b16a7dbd50dd8077bed9f17174545d67d24
_sebzx <sebapico05@gmail.com>
2024-07-08 19:50:41 -0500

    Agregamos cambios locales

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function verificar(){
-    if(document.getElementById('email').value.length < 4){
-        alert("el correo debe tener al menos 8 caracteres");
-    }
-}
+const username = document.querySelector("#username")
+const password = document.querySelector("#pass")
+const btn = document.querySelector("#boton")
+
+btn.addEventListener("click", function (event) {
+  event.preventDefault()
+  if (username.value == "1") {
+    username.classList.add("errorField")
+  }
+  if (password.value == "1") {
+    password.classList.add("errorField")
+  }
+})
+
+username.addEventListener("keyup", function () {
+  username.classList.remove("errorField")
+})
+password.addEventListener("keyup", function () {
+  password.classList.remove("errorField")
+})
